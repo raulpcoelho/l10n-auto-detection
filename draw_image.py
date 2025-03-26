93a1576e9203d14f9892f7a134b2931cd7a9b72b
Raul Coelho <rpc3@cin.ufpe.br>
2025-03-26 12:47:50 -0300

    fix: save only highlighted images

--- draw_image.py
+++ draw_image.py
@@ -9,12 +9,14 @@ def draw_image(image_path, output_images_path, words_to_highlight):
 
     data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
 
-    draw = ImageDraw.Draw(image)
+    highlighted = False
     for i, word in enumerate(data["text"]):
         if len(word) == 0:
             continue
         if word.find("...") != -1:
-            print(f"Found ellipsis on word: {word} at index: {i}")
+            highlighted = True
+            draw = ImageDraw.Draw(image)
+            print(f"Highlighting ellipsis on word: {word}")
             x, y, w, h = (
                 data["left"][i],
                 data["top"][i],
@@ -29,7 +31,10 @@ def draw_image(image_path, output_images_path, words_to_highlight):
 
             draw.rectangle([x, y, x + w, y + h], outline="red", width=2)
         if word in words_to_highlight:
-            print(f"Highlighting word: {word} at index: {i}")
+            highlighted = True
+            draw = ImageDraw.Draw(image)
+
+            print(f"Highlighting word: {word}")
 
             x, y, w, h = (
                 data["left"][i],
@@ -38,8 +43,8 @@ def draw_image(image_path, output_images_path, words_to_highlight):
                 data["height"][i],
             )
             draw.rectangle([x, y, x + w, y + h], outline="red", width=2)
-
-    os.makedirs(output_images_path, exist_ok=True)
-    output_path = output_images_path + "/" + image_path.split("/")[-1]
-    image.save(output_path)
-    print(f"Annotated image saved to: {output_path}")
+    if highlighted:
+        os.makedirs(output_images_path, exist_ok=True)
+        output_path = output_images_path + "/" + image_path.split("/")[-1]
+        image.save(output_path)
+        print(f"Annotated image saved to: {output_path}")
