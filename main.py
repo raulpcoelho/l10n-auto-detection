2ba3a7e3191bcfee5f9ce95a5937a921f0772914
Raul Coelho <rpc3@cin.ufpe.br>
2025-03-31 00:23:59 -0300

    feat: add spanish dict

--- main.py
+++ main.py
@@ -16,16 +16,25 @@ parser.add_argument("--apk-path", "-a", help="APK file path", required=True)
 parser.add_argument(
     "--skip-droidbot", "-s", action="store_true", help="Skip droidbot exploration"
 )
+parser.add_argument("--lang", "-l", help="App language", default="pt-br")
+parser.add_argument(
+    "--custom-ignore", "-i", help="Custom ignore file path", default=None
+)
 args = parser.parse_args()
 
 
 acceptable_words = []
-with open("dictionaries/pt-br.txt", "r", encoding="utf-8") as file:
+lang = args.lang
+with open(f"dictionaries/{lang}.txt", "r", encoding="utf-8") as file:
     acceptable_words = [line.strip() for line in file]
 
 ignore_words = []
-with open("dictionaries/ignore_pt-br.txt", "r", encoding="utf-8") as file:
+with open(f"dictionaries/ignore_{lang}.txt", "r", encoding="utf-8") as file:
     ignore_words = [line.strip() for line in file]
+
+if args.custom_ignore:
+    with open(args.custom_ignore, "r", encoding="utf-8") as file:
+        ignore_words += [line.strip() for line in file]
 
 if not args.skip_droidbot:
     run_droidbot(args.apk_path, args.exploration_time, "droidbot_dump")
